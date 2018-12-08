82f31d8535a5fdd1b9afa6c4c4bc377cfd48fd63
Tomasz Kolinko <kolinko@gmail.com>
2018-12-08 09:33:48 +0800

    api caching

--- parse.py
+++ parse.py
@@ -2,10 +2,28 @@ import urllib
 import urllib.request
 import json
 
-url = "http://eveem.org/code/0x2Ad180cBAFFbc97237F572148Fc1B283b68D8861.json"
+import os
 
-with urllib.request.urlopen(url) as response:
-    re = response.read()
-    res = json.loads(re)
+address = "0x2Ad180cBAFFbc97237F572148Fc1B283b68D8861"
+url = f"http://eveem.org/code/{address}.json"
+cache_fname = f'cache/{address}.json'
 
-    print(res)
+print(url)
+
+
+if os.path.isfile(cache_fname):
+    with open(cache_fname) as f:
+        contract = json.loads(f.read())
+else:
+
+    with urllib.request.urlopen(url) as response:
+        re = response.read()
+        contract = json.loads(re)
+
+        with open(fname, 'w') as f:
+            f.write(json.dumps(contract, indent=2))
+
+functions = contract['functions']
+
+for f in functions:
+    print(f['color_name'])
